1c8f2bd7a5ffc565f1bb074a39d22fe7097360d1
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-18 21:39:10 +0300

    add: comment

--- turbojwt.go
+++ turbojwt.go
@@ -8,11 +8,18 @@ import (
 	"fmt"
 	"strings"
 )
-
+// base64Encode allow to generate base64.URLEncoding
 func base64Encode(data []byte) string {
 	return base64.RawURLEncoding.EncodeToString(data)
 }
-
+// generateHmac generate a hashed data for the jwt signature
+func generateHmac(secret string, unsignedToken string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(unsignedToken))
+	return base64Encode(h.Sum(nil))
+}
+// Encode generate jwt token by using the secret and payload variable
 func Encode(secret string, playload map[string]interface{}) (string, error) {
 	header := map[string]string{
 		"alg": "HS256",
@@ -30,12 +37,7 @@ func Encode(secret string, playload map[string]interface{}) (string, error) {
 	return unsignedToken + "." + signature, nil
 }
 
-func generateHmac(secret string, unsignedToken string) string {
-	key := []byte(secret)
-	h := hmac.New(sha256.New, key)
-	h.Write([]byte(unsignedToken))
-	return base64Encode(h.Sum(nil))
-}
+
 
 func Verify(secret string, token string) (bool, error) {
 	parts := strings.Split(token, ".")
