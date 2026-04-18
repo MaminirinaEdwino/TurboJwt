4550cdc4a6894bc874c3c96e19cd9a1cd95e12c6
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-18 20:30:30 +0300

    implement: encode function

--- turbojwt.go
+++ turbojwt.go
@@ -1,6 +1,8 @@
 package turbojwt
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
 )
@@ -10,11 +12,22 @@ func base64Encode(data []byte) string {
 }
 
 func Encode(secret string, playload map[string]interface{}) (string, error) {
-	header := map[string]string {
+	header := map[string]string{
 		"alg": "HS256",
-		"typ":"JWT",
+		"typ": "JWT",
 	}
 
 	headerJson, _ := json.Marshal(header)
 	encodedHeader := base64Encode(headerJson)
+
+	payloadJson, _ := json.Marshal(playload)
+	encodedPayload := base64Encode(payloadJson)
+
+	unsignedToken := encodedHeader + "." + encodedPayload
+
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(unsignedToken))
+	signature := base64Encode(h.Sum(nil))
+	return unsignedToken + "." + signature, nil
 }
