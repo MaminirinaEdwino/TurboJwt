bf1e0b0a34d2f6d3251e943ceb3d7ec3d0343b3d
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-04-18 20:30:58 +0300

    clean

--- turbojwt.go
+++ turbojwt.go
@@ -16,15 +16,11 @@ func Encode(secret string, playload map[string]interface{}) (string, error) {
 		"alg": "HS256",
 		"typ": "JWT",
 	}
-
 	headerJson, _ := json.Marshal(header)
 	encodedHeader := base64Encode(headerJson)
-
 	payloadJson, _ := json.Marshal(playload)
 	encodedPayload := base64Encode(payloadJson)
-
 	unsignedToken := encodedHeader + "." + encodedPayload
-
 	key := []byte(secret)
 	h := hmac.New(sha256.New, key)
 	h.Write([]byte(unsignedToken))
